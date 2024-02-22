cf99d73319290c33d8efc8ed8c018d4a07cbbb6f
Yomi Oke <okeoluwayomi9@gmail.com>
2024-02-22 17:12:43 +0100

    add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,42 +1,47 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import moment from "moment";
 
 const Footer = () => {
+  const currentYear = moment().format("YYYY");
   return (
-    <div className="max-w-1440px max-h-400px ">
-        <div className="w-1312px h-122px pt-16 px-16 ">
-            <div className="w-236px h-90.57px flex flex-col ">
-                <Image 
-                alt='alt Image'
-                width={200}
-                height={34.57}
-                src={""}
-                className=""/>
-
-                <div className="w-236px flex  mt-6 space-x-12">
-                    <Link href={""}>link one</Link>
-                    <Link href={""}>link two</Link>
-                    <Link href={""}>link three</Link>
-                    <Link href={""}>link four</Link>
-                    <Link href={""}>link five</Link>
-                </div>
-            </div>
+    <div className="max-w-screen-xxl px-16 py-20 w-full mx-auto">
+      <div className="">
+        <div className="mb-8">
+          <Image
+            alt="alt Image"
+            width={200}
+            height={34.57}
+            src={"/logo.svg"}
+            className=""
+          />
         </div>
 
-
-        <div className="h-54px w-1312px px-16 flex items-baseline mt-6 justify-between pb-16">
-            <div className="flex space-x-4">
-                <p>Privacy Policy</p>
-                <p>Terms of service</p>
-                <p>Cookies Settings</p>
-            </div>
-            <div>
-                <p>@2024 Daily Times Nigeria. All rights reserved</p>
-            </div>
+        <div className="flex items-center gap-8">
+          {["one", "two", "three", "four", "five"].map((link) => (
+            <Link key={link} href={""} className="font-semibold capitalize">
+              Link {link}
+            </Link>
+          ))}
         </div>
+      </div>
+
+      <div className="mt-16 flex items-center justify-between pt-8 border-t border-custom-black">
+        <div className="flex items-center flex-wrap gap-6 text-custom-black text-sm">
+          <p className="underline">Privacy Policy</p>
+          <p className="underline">Terms of service</p>
+          <p className="underline">Cookies Settings</p>
+        </div>
+
+        <div>
+          <p className="text-custom-black text-sm">
+            @{currentYear} Daily Times Nigeria. All rights reserved
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
