b74d1d5d77f4fa605b76cb1ae1a53b80aaf1ecbf
Yomi Oke <okeoluwayomi9@gmail.com>
2024-02-22 19:12:39 +0100

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,144 +1,118 @@
-'use client';
+"use client";
 
-import { navLinks } from '@/data/navigation'
-import React, { useState } from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useSelectedLayoutSegment } from 'next/navigation'
-import { Button } from './ui/button';
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { useSelectedLayoutSegment } from "next/navigation";
 
-const Navbar = () => {
-    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
-
-    const toggleMobileMenu = () => {
-      setMobileMenuOpen(!isMobileMenuOpen);
-    };
-  
-
-    const segment = useSelectedLayoutSegment();
-  return (
-        
-    <header className="h-20 px-100px bg-white text-custom-black z-10 border-gray-400 ">
-
-        <nav className="w-full h-full py-5 lg:py-3 lg:px-60px  px-6">
-                
-            <div className="container mx-auto flex items-center justify-between   ">
-                <div>
-                    <Link href={"/"}>
-                        <Image
-                            src={""}
-                            alt={"logo"}
-                            width={70}
-                            height={40}
-                        />
-                    </Link>
-                </div>
-
-                <div className="hidden lg:flex space-x-4">
-                    <div className="flex flex-col justify-end gap-3">
-                        <ul className="flex items-center gap-14 justify-end transition-all duration-300 ease-in-out">
-                            {navLinks.map((nav, index) => {
-                            const isActive = segment === nav.url;
-                            return (
-                                <li key={index}>
-                                <Link href={`/${nav.url}`}>
-                                    <p
-                                    className={`text-sm hover:text-custom-green transition-all duration-300 cursor-pointer ${
-                                        isActive
-                                        ? "text-custom-green"
-                                        : "text-custom-darkgray"
-                                    }`}
-                                    >
-                                    {nav.link}
-                                    </p>
-                                </Link>
-                                </li>
-                            );
-                            })}
-                        </ul>
-
-                    <div>
-                <div>
-                    <div>
-                        <Button className="bg-black text-white rounded-lg">
-                            SUBMIT ASTORY
-                        </Button>
-                    </div>
-                </div>
-                        
-                    </div>
-                </div>
-            
-            {/* {mobile navigation} */}
-            <div className="lg:hidden">
-            <button
-              className="text-white focus:outline-none transition-all duration-300 ease-in-out rounded-full bg-green-600 "
-              onClick={toggleMobileMenu}
-            >
-                Button
-              {/* {isMobileMenuOpen ? (
-                <Image
-                  src={"/svgs/close.svg"}
-                  alt={"close"}
-                  width={36}
-                  height={36}
-                  className="w-auto"
-                />
-              ) : (
-                <Image
-                  src={"/svgs/open.svg"}
-                  alt={"open"}
-                  width={36}
-                  height={36}
-                  className="w-auto"
-                />
-              )} */}
-            </button>
-
-
-            <div
-              className={`absolute top-20 bg-white w-full h-full z-10 border-t border-custom-white transform transition-transform duration-300 ease-in-out ${
-                isMobileMenuOpen ? "right-0" : "translate-x-full"
-              }`}
-            >
-              <div className="px-6 py-12">
-                <ul className="flex flex-col gap-10">
-                  {navLinks.map((nav, index) => {
-                    const isActive = segment === nav.url;
-
-                    return (
-                      <li key={index}>
-                        <Link
-                          href={`/${nav.url}`}
-                          onClick={() => {
-                            setMobileMenuOpen(false);
-                          }}
-                        >
-                          <p
-                            className={`hover:text-custom-green transition-all duration-300 ${
-                              isActive
-                                ? "text-custom-green"
-                                : "text-custom-darkgray"
-                            }`}
-                          >
-                            {nav.link}
-                          </p>
-                        </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
-
-              </div>
-            </div>
-            </div>
-            </div>
-                </div>
-        </nav>
-    </header>
-
- 
-  )
+export interface IAffiliatedLinks {
+  link: string;
+  url: string;
 }
 
-export default Navbar
+const Navbar = () => {
+  const segment = useSelectedLayoutSegment();
+
+  const [open, setOpen] = useState(false);
+  const toggleMobileMenu = () => {
+    setOpen(!open);
+  };
+
+  const navbarHeight = 75;
+  const fullScreenHeight = `calc(100vh - ${navbarHeight}px)`;
+
+  return (
+    <div className="bg-white shadow-md w-full transition-all duration-300 ease-in-out">
+      <div
+        className={`lg:flex items-center justify-between py-4 lg:px-20 px-6 bg-yellow-400`}
+      >
+        <div className="bg-red-500">
+          <Link href={"/"}>
+            <Image
+              alt="alt Image"
+              width={200}
+              height={34.57}
+              src={"/logo.svg"}
+              className=""
+            />
+          </Link>
+          <p className="font-semibold text-sm mt-0">Since 1926</p>
+        </div>
+
+        <div
+          onClick={toggleMobileMenu}
+          className="absolute right-6 top-1/2 -translate-y-1/2 cursor-pointer lg:hidden"
+        >
+          {open ? <p>close</p> : <p>open</p>}
+        </div>
+
+        <ul
+          className={`lg:flex lg:items-center lg:pb-0 pb-12 absolute lg:static bg-white lg:bg-inherit lg:z-auto left-0 w-full lg:w-auto lg:pl-0 pl-9 transition-all duration-500 ease-in-out ${
+            open
+              ? "top-[75px] opacity-100 z-50 lg:z-auto"
+              : "top-[-490px] lg:opacity-100 opacity-0 z-[-1] lg:z-auto"
+          }`}
+          style={{ height: open ? fullScreenHeight : "" }}
+        >
+          {navLinks.map((nav, index) => {
+            const isActive = segment === nav.url;
+            const isHome = nav.url === "#";
+
+            return (
+              <li key={index} className="lg:ml-8 lg:my-0 my-7">
+                <Link
+                  href={`/${nav.url}`}
+                  className={`text-sm hover:text-custom-green transition-all ease-in-out duration-300 cursor-pointer font-light ${
+                    isActive || (isHome && segment === null)
+                      ? "text-custom-green font-medium"
+                      : "text-custom-darkgray lg:text-white"
+                  }`}
+                  onClick={() => setOpen(false)}
+                >
+                  {nav.link}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
+
+export const navLinks: IAffiliatedLinks[] = [
+  {
+    link: "Home",
+    url: "#",
+  },
+  {
+    link: "About Us",
+    url: "about-us",
+  },
+  {
+    link: "Our Brands",
+    url: "our-brands",
+  },
+  {
+    link: "Sustainability",
+    url: "sustainability",
+  },
+  {
+    link: "News/Media",
+    url: "news",
+  },
+  {
+    link: "Career",
+    url: "career",
+  },
+  {
+    link: "Contact",
+    url: "contact",
+  },
+  {
+    link: "Buy Now",
+    url: "buy-now",
+  },
+];
