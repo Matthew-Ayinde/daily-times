d4c40aaa5197e00b9ed05d2c0f86976d3d328724
Matthew Ayinde <ayindematthew2003@gmail.com>
2024-02-26 19:14:19 +0100

    merging

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
-import HomeCarousel from "@/components/HomeCarousel";
-import HomePage from "@/components/HomePage";
+import HomeCarousel from '@/components/HomeCarousel';
+import HomePage from '@/components/HomePage';
+import Story from '@/components/Story';
+// import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
     <div className="">
       <HomePage />
 
+      {/* <Banner /> */}
+
       <HomeCarousel />
+
+      <Story />
     </div>
   );
 }
