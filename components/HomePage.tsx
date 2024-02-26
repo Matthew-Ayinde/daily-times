3162f2604f88827f37043191aebf49d38ee36228
lawal069 <lawaloluwaseyi069@gmail.com>
2024-02-26 19:16:52 +0100

    skeleton component on Trending section

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -6,9 +6,11 @@ import AdvertismentSection from "./AdvertismentSection";
 import { IArticle } from "@/types/articles";
 import axios from "axios";
 import { BASE_URL } from "@/lib/constants";
+import { Skeleton } from "@/components/ui/skeleton";
 import ContentCard from "./ContentCard";
 import Banner from "./Banner";
 import NewCardComponent from "./NewCardComponent";
+import { Card, CardHeader, CardContent } from "./ui/card";
 
 const HomePage = () => {
   const isFetching = useIsFetching();
@@ -68,13 +70,47 @@ const HomePage = () => {
             Trending
           </p>
 
-          {isSuccess && articlesData && (
+          {isLoading || isFetching ? (
+            <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+              {Array(9)
+                .fill(null)
+                .map((box) => (
+                  // <Skeleton
+                  //   key={box}
+                  //   className="w-[350px] h-60 bg-gray-300"
+                  // />
+                  <div key={box}>
+                    <div className="flex flex-col w-[350px]">
+                      <Skeleton className="w-full h-60 bg-gray-300" />
+                      <div className="min-h-100px flex flex-col items-center justify-center mt-4">
+                        <Skeleton className="h-[14px] w-[46px] rounded-xl bg-gray-300 mt-4" />
+                        <Skeleton className="h-[14px] w-[289px] rounded-xl bg-gray-300 mt-4" />
+                        <Skeleton className="h-[14px] w-[78px] rounded-xl bg-gray-300 mt-4" />
+                        <Skeleton className="h-[14px] w-[76px] rounded-xl bg-gray-300 mt-4" />
+                      </div>
+                    </div>
+                  </div>
+                ))}
+            </ul>
+          ) : (
+            <>
+              {isSuccess && articlesData && (
+                <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+                  {articlesData.map((article) => (
+                    <NewCardComponent article={article} key={article.id} />
+                  ))}
+                </ul>
+              )}
+            </>
+          )}
+
+          {/* {isSuccess && articlesData && (
             <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
               {articlesData.map((article) => (
                 <NewCardComponent article={article} key={article.id} />
               ))}
             </ul>
-          )}
+          )} */}
         </div>
       </>
     </div>
