0238ac25fbdc4bb443d71a1e44b017bd8de50b30
Yomi Oke <okeoluwayomi9@gmail.com>
2024-02-27 09:23:15 +0100

    update stale time refresh

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -45,7 +45,7 @@ const HomePage = () => {
   } = useQuery<IArticle[], Error>({
     queryKey: ["articles"],
     queryFn: fetchArticles,
-    staleTime: 3600000,
+    staleTime: 5000,
   });
 
   // if (isFetching) return <div>Fetching...</div>;
